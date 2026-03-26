6c7756f19692405299b9dadc48e4ba0ff39d8d17
Krish Shah <krishcshah09@gmail.com>
2026-03-26 12:04:50 +0530

    added min max for stats and getter for all students

--- analyzer.cpp
+++ analyzer.cpp
@@ -2,6 +2,7 @@
 #include<string>
 #include <curl/curl.h>
 #include "json.hpp"
+#include<iomanip>
 using json = nlohmann::json;
 using namespace std;
 class Student{
@@ -22,15 +23,85 @@ public:
     }
     void getStudent(){
         cout<<"Username="<<username<<endl;
+        cout<<"Name="<<name<<endl;
         cout<<"Description="<<description<<endl;
         cout<<"Followers="<<followers<<endl;
         cout<<"Following="<<following<<endl;
         cout<<"Repos="<<repos<<endl;
     }
+    int getFollowers(){
+        return followers;
+    }
+    int getFollowing(){
+        return following;
+    }
+    int getRepos(){
+        return repos;
+    }
 };
 size_t WriteCallback(void* contents, size_t size, size_t nmemb, void* userp) {
     ((string*)userp)->append((char*)contents, size * nmemb);
     return size * nmemb;
+}
+int maximumFollowers(int n,Student s[]){
+    int maximum=0;
+    for(int i=0;i<n;i++){
+        int followers=s[i].getFollowers();
+        if(followers>maximum){
+            maximum=followers;
+        }
+    }
+    return maximum;
+}
+int minimumFollowers(int n,Student s[]){
+    int minimum=9999999;
+    for(int i=0;i<n;i++){
+        int followers=s[i].getFollowers();
+        if(followers<minimum){
+            minimum=followers;
+        }
+    }
+    return minimum;
+}
+int maximumFollowing(int n,Student s[]){
+    int maximum=0;
+    for(int i=0;i<n;i++){
+        int following=s[i].getFollowing();
+        if(following>maximum){
+            maximum=following;
+        }
+    }
+    return maximum;
+}
+int minimumFollowing(int n,Student s[]){
+    int minimum=9999999;
+    for(int i=0;i<n;i++){
+        int following=s[i].getFollowing();
+        if(following<minimum){
+            minimum=following;
+        }
+    }
+    return minimum;
+}
+int maximumRepos(int n,Student s[]){
+    int maximum=0;
+    for(int i=0;i<n;i++){
+        int repos=s[i].getRepos();
+        if(repos>maximum){
+            maximum=repos;
+        }
+    }
+    return maximum;
+}
+int minimumRepos(int n,Student s[]){
+    int minimum=9999999;
+    for(int i=0;i<n;i++){
+        int repos=s[i].getRepos();
+        if(repos<minimum){
+            minimum=repos;
+        }
+    }
+    return minimum;
 }
 int main(){
     CURL *curl;
@@ -39,7 +110,7 @@ int main(){
 
     curl=curl_easy_init();
     if(curl==NULL){
-        cout<<"failed";
+        cout<<"failed"<<endl;
         return 0;
     }
     curl_easy_setopt(curl, CURLOPT_USERAGENT, "C++ App");
@@ -64,20 +135,60 @@ int main(){
         }
         json data = json::parse(response);
         if(data["message"]=="Not Found"){
-            cout<<endl<<"Not found username"<<endl;;
+            cout<<"Not found username"<<endl;;
             i--;
             continue;
         }
         char login[100];
         char name_c[50];
         char bio_c[160];
-        strcpy(login,data["login"].get<string>().c_str());
-        strcpy(name_c,data["name"].get<string>().c_str());
-        strcpy(bio_c,data["bio"].get<string>().c_str());
-        s[i].setStudent(login,name_c,bio_c,data["followers"],data["following"],data["public_repos"]);
-        s[i].getStudent();
+        strcpy(login,data["login"].is_null()?"":data["login"].get<string>().c_str());
+        strcpy(name_c,data["name"].is_null()?"":data["name"].get<string>().c_str());
+        strcpy(bio_c,data["bio"].is_null()?"":data["bio"].get<string>().c_str());
+        s[i].setStudent(
+            login,
+            name_c,
+            bio_c,
+            data["followers"],
+            data["following"],
+            data["public_repos"]);
     }
-
+    cout<<"Enter what details you want to view"<<endl;
+    while(1){
+        int choice;
+        cout<<"------------------CHOOSE YOUR CHOICE------------------"<<endl;
+        cout<<setw(10)<<1<<"\t View details of all users"<<endl; 
+        cout<<setw(10)<<2<<"\t Maximum and Minimum Followers,following,repos"<<endl; 
+        cout<<setw(10)<<0<<"\t Exit program"<<endl; 
+        cin>>choice;
+        if(choice==0){
+            break;
+        }else if(choice==1){
+            cout<<endl;
+            for(int i=0;i<n;i++){
+                cout<<endl;
+                s[i].getStudent();
+                cout<<endl;
+            }
+        }else if(choice==2){
+            int maxfr=maximumFollowers(n,s);
+            int minfr=minimumFollowers(n,s);
+            int maxfl=maximumFollowing(n,s);
+            int minfl=minimumFollowing(n,s);
+            int maxr=maximumRepos(n,s);
+            int minr=minimumRepos(n,s);
+            cout<<endl;
+            cout<<setw(20)<<"Maximum Followers"<<"\t"<<maxfr<<endl;
+            cout<<setw(20)<<"Minimum Followers"<<"\t"<<minfr<<endl;
+            cout<<setw(20)<<"Maximum Following"<<"\t"<<maxfl<<endl;
+            cout<<setw(20)<<"Minimum Following"<<"\t"<<minfl<<endl;
+            cout<<setw(20)<<"Maximum Repos"<<"\t"<<maxr<<endl;
+            cout<<setw(20)<<"Minimum Repos"<<"\t"<<minr<<endl;
+            cout<<endl;
+        }else{
+            cout<<"Invalid choice"<<endl;
+        }
+    }
 
     curl_easy_cleanup(curl);
 }
